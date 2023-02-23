61ca0057bc1b2939c5049b5dda2c184e5b82621a
Nicolas-Orozco <nicolasorozcodavila@gmail.com>
2023-02-22 23:16:42 -0500

    Add code from assessment

--- my_first_script.R
+++ my_first_script.R
@@ -1,11 +1,15 @@
-library(tidyverse)
-library(ggplot2)
-library(dslabs)
-data(murders)
+#library(tidyverse)
+#library(ggplot2)
+#library(dslabs)
+#data(murders)
 
-murders %>%
-  ggplot(aes(population, total, label=abb, color=region)) +
-  geom_label()
-
+#murders %>%
+#  ggplot(aes(population, total, label=abb, color=region)) +
+#  geom_label()
+name <- c("Mandi", "Amy", "Nicole", "Olivia")
+distance <- c(0.8, 3.1, 2.8, 4.0)
+time <- c(10, 30, 40, 50)
+time <- time/60
+speed <- distance/time
 
 
