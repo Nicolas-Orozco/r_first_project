142ac1a260218f1ae87d13c5b70424b8b3572a61
Nicolas-Orozco <nicolasorozcodavila@gmail.com>
2023-02-23 22:43:37 -0500

    Add code from murders_rate and west regions safety

--- my_first_script.R
+++ my_first_script.R
@@ -1,15 +1,19 @@
 #library(tidyverse)
 #library(ggplot2)
-#library(dslabs)
-#data(murders)
-
 #murders %>%
 #  ggplot(aes(population, total, label=abb, color=region)) +
 #  geom_label()
-name <- c("Mandi", "Amy", "Nicole", "Olivia")
-distance <- c(0.8, 3.1, 2.8, 4.0)
-time <- c(10, 30, 40, 50)
-time <- time/60
-speed <- distance/time
+#name <- c("Mandi", "Amy", "Nicole", "Olivia")
+#distance <- c(0.8, 3.1, 2.8, 4.0)
+#time <- c(10, 30, 40, 50)
+#time <- time/60
+#speed <- distance/time
 
+library(dslabs)
+data(murders)
+murder_rate <- murders$total/murders$population*100000
+safe <- murder_rate <= 1
+west <- murders$region == "West"
+index <- safe & west
+murders$state[index]
 
