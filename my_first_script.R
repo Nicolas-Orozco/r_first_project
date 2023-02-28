2c106f31307cf973104c16685422dd69ebfbe6a0
Nicolas-Orozco <nicolasorozcodavila@gmail.com>
2023-02-27 23:36:22 -0500

    Add data frame example

--- my_first_script.R
+++ my_first_script.R
@@ -24,4 +24,10 @@ library(dplyr)
 #new_table <- select(murders,state,region,rate)
 #filter(new_table, rate <= 0.71)
 
-murders %>% select(state,region,rate) %>% filter(rate <= 0.71)
+#murders %>% select(state,region,rate) %>% filter(rate <= 0.71)
+
+# creating a data frame with stringAsFactors = FALSE
+grades <- data.frame(names = c("John", "Juan", "Jean", "Yao"), 
+                     exam_1 = c(95, 80, 90, 85), 
+                     exam_2 = c(90, 85, 85, 90),
+                     stringsAsFactors = FALSE)
